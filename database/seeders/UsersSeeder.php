dbab4b4f028d8df60e7d3721a622af5cc0819576
alexaaqil <alexaaqil.se@gmail.com>
2025-02-03 10:24:53 +0300

    Users Seeder: modified the user seeder to only generate real users.

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -14,49 +14,49 @@ class UsersSeeder extends Seeder
     public function run(): void
     {
         // Create 5 real users with specific details
-        $admin_password = 'r00t';
+        $admin_password = '@dmin';
         $user_password = 'password';
 
         $realUsers = [
             [
                 'first_name' => 'Admin',
                 'last_name' => 'Root',
-                'email' => 'admin@gmail.com',
+                'email' => 'admin_root@gmail.com',
                 'password' => $admin_password,
                 'user_level' => 0,
             ],
             [
                 'first_name' => 'Admin',
                 'last_name' => 'Admin',
-                'email' => 'admin1@gmail.com',
-                'password' => $user_password,
+                'email' => 'admin@gmail.com',
+                'password' => $admin_password,
                 'user_level' => 1,
             ],
             [
                 'first_name' => 'Jane',
                 'last_name' => 'Smith',
-                'email' => 'jane.smith@example.com',
+                'email' => 'jane@gmail.com',
                 'password' => $user_password,
             ],
             [
-                'first_name' => 'Michael',
-                'last_name' => 'Brown',
-                'email' => 'michael.brown@example.com',
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'email' => 'john@gmail.com',
                 'password' => $user_password,
             ],
             [
-                'first_name' => 'Emily',
+                'first_name' => 'User',
                 'last_name' => 'Johnson',
-                'email' => 'emily.johnson@example.com',
+                'email' => 'user@gmail.com',
                 'password' => $user_password,
             ],
         ];
 
         foreach ($realUsers as $user) {
-            User::factory()->create($user);
+            User::create($user);
         }
 
         // Create 10 fake users
-        User::factory(10)->create();
+        // User::factory(10)->create();
     }
 }
