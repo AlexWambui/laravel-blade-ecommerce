38d71f361b5f174fe6c58ea7f1be6eeae55d1bac
AlexAaqil <alexaaqil.se@gmail.com>
2025-03-11 00:10:08 +0300

    Dashboard: added the logic for displaying the accurate dashboard details.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,26 +5,99 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Message;
+use App\Models\Deliveries\DeliveryArea;
+use App\Models\Deliveries\DeliveryLocation;
+use App\Models\Products\Product;
+use App\Models\Products\ProductCategory;
+use App\Models\Sales\Sale;
+use App\Models\Sales\SaleDelivery;
+use App\Models\Sales\SaleItem;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use PDO;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $user = Auth()->user();
+        $user = Auth::user();
+    
         $count_users = User::whereNotIn('user_level', [0, 1])
             ->where('user_status', 1)
             ->count();
         $count_all_users = User::count();
+    
         $messages = Message::latest()
             ->where('status', 0)
             ->take(5)
             ->get();
+        $count_all_messages = Message::count();
+    
+        $count_products = Product::count();
+        $count_product_categories = ProductCategory::count();
+    
+        $count_locations = DeliveryLocation::count();
+        $count_areas = DeliveryArea::count();
+    
+        $count_sales = Sale::count();
 
+
+        $databaseDriver = DB::connection()->getPDO()->getAttribute(PDO::ATTR_DRIVER_NAME);
+        $monthFunction = match ($databaseDriver) {
+            'sqlite' => "CAST(strftime('%m', created_at) AS INTEGER)",
+            default => "MONTH(created_at)",
+        };
+        
+        // Sales Statistics
+        $gross_sales = Sale::sum('total_amount');
+        $net_sales = Sale::sum('total_amount') - Sale::sum('discount');
+        $cost_of_sales = SaleItem::sum('buying_price');
+        $gross_profit = $net_sales - $cost_of_sales;
+    
+        // Sales for each month
+        $monthly_sales = Sale::selectRaw("$monthFunction as month, SUM(total_amount) as total_sales")
+        ->groupBy('month')
+        ->orderBy('month')
+        ->pluck('total_sales', 'month');
+    
+        $sales_data = [];
+        for ($month = 1; $month <= 12; $month++) {
+            $sales_data[] = $monthly_sales[$month] ?? 0;
+        }
+
+
+        $locations_data = SaleDelivery::select('location', DB::raw('COUNT(*) as total_orders'))
+            ->groupBy('location')
+            ->orderBy('total_orders', 'desc')
+            ->get();
+
+        // Map the data for cities and orders
+        $locations_labels = $locations_data->pluck('location')->toArray();
+        $locations_orders = $locations_data->pluck('total_orders')->toArray();
+    
         return view('dashboard.index', compact(
             'user',
+
             'count_users',
             'count_all_users',
+            'count_products',
+            'count_product_categories',
+            'count_locations',
+            'count_areas',
+            'count_all_messages',
+            'count_sales',
+
             'messages',
+            'gross_sales',
+            'net_sales',
+            'cost_of_sales',
+            'gross_profit',
+            
+            'sales_data',
+            'locations_labels',
+            'locations_orders',
         ));
     }
+    
 }
--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -6,29 +6,65 @@
         </div>
 
         <div class="stat">
-            <span>{{ $count_users }}</span>
-            <span>Out of {{ $count_all_users }} users</span>
+            <span>{{ $count_products }}</span>
+            <span>Products and {{ $count_product_categories }} categories</span>
         </div>
 
         <div class="stat">
-            <span>{{ $count_users }}</span>
-            <span>Out of {{ $count_all_users }} users</span>
+            <span>{{ $count_sales }}</span>
+            <span>Total Sales</span>
         </div>
 
         <div class="stat">
-            <span>{{ $count_users }}</span>
-            <span>Out of {{ $count_all_users }} users</span>
+            <span>{{ $count_locations }}</span>
+            <span>Locations and {{ $count_areas }} areas</span>
         </div>
 
         <div class="stat">
-            <span>{{ $count_users }}</span>
-            <span>Out of {{ $count_all_users }} users</span>
+            <span>{{ $count_all_messages }}</span>
+            <span>Total Messages</span>
+        </div>
+    </div>
+
+    <div class="section stats sales">
+        <div class="stat">
+            <span>{{ number_format($gross_sales, 0) }}</span>
+            <span>Gross Sales</span>
+        </div>
+
+        <div class="stat">
+            <span>{{ number_format($net_sales, 0) }}</span>
+            <span>Net Sales</span>
+        </div>
+
+        <div class="stat">
+            <span>{{ number_format($cost_of_sales, 0) }}</span>
+            <span>Cost of Sales</span>
+        </div>
+
+        <div class="stat">
+            <span>{{ number_format($gross_profit, 0) }}</span>
+            <span>Gross Profit</span>
+        </div>
+    </div>
+
+    <div class="section charts">
+        <div class="charts">
+            <div class="chart">
+                <h2>Sales</h2>
+                <canvas id="salesChart"></canvas>
+            </div>
+
+            <div class="chart">
+                <h2>Order Locations</h2>
+                <canvas id="citiesChart"></canvas>
+            </div>
         </div>
     </div>
 
     <div class="section messages">
         <p class="title">Unread Messages</p>
-        @foreach($messages as $message)
+        @forelse($messages as $message)
             <div class="message">
                 <p class="stack">
                     <a href="{{ route('messages.edit', $message->id) }}">
@@ -37,6 +73,49 @@
                     <span>{{ $message->name }}</span>
                 </p>
             </div>
-        @endforeach
+        @empty
+            <p>There's not unread messages</p>
+        @endforelse
     </div>
+
+    <x-slot name="scripts">
+        <script src="{{ asset('assets/js/chart.js') }}"></script>
+        <script>
+            const ctx = document.getElementById('salesChart');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                    datasets: [{
+                        label: 'Amount',
+                        data: {!! json_encode($sales_data) !!},
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                }
+            });
+
+            const cities = document.getElementById('citiesChart');
+            new Chart(cities, {
+                type: 'doughnut',
+                data: {
+                    labels: {!! json_encode($locations_labels) !!},
+                    datasets: [{
+                        label: 'Orders',
+                        data: {!! json_encode($locations_orders) !!},
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'right',
+                        }
+                    }
+                }
+            });
+        </script>
+    </x-slot>
 </section>
